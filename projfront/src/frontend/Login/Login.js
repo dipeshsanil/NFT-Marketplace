36171194d3b24cbd798af26c5839fc2b6e1ac8ec
DiveshSanil <diveshdipesh1@gmail.com>
2022-04-16 18:36:42 +0530

    Update Login.js

--- projfront/src/frontend/Login/Login.js
+++ projfront/src/frontend/Login/Login.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import {ethers} from 'ethers'
 import "../style.css"
-import { Link,Outlet } from "react-router-dom";
-
+import {Outlet,useNavigate } from "react-router-dom";
 import Button from "../Button";
+
 const Login = ()=>{
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+	const [provider, setProvider] = useState(null);
+
+    const connectWalletHandler = () => {
+		if (window.ethereum && defaultAccount == null) {
+			// set ethers provider
+			setProvider(new ethers.providers.Web3Provider(window.ethereum));
+
+			// connect to metamask
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				setConnButtonText('Wallet Connected');
+				setDefaultAccount(result[0]);
+                navigate("/home");
+			})
+			.catch(error => {
+                alert(error.message);
+				setErrorMessage(error.message);
+			});
+
+		} else if (!window.ethereum){
+			console.log('Need to install MetaMask');
+            if(!alert('Please install MetaMask browser extension to interact')){
+                window.open("https://metamask.io/download/");
+            }
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+    useEffect(() => {
+        if(defaultAccount){
+        provider.getBalance(defaultAccount)
+        .then(balanceResult => {
+            setUserBalance(ethers.utils.formatEther(balanceResult));
+        })
+        };
+    }, [defaultAccount]);
+
     return (
         <>
                 <div className="container-fluid">
@@ -14,10 +57,11 @@ const Login = ()=>{
                         <h2>Welcome Back!</h2>
                         <p>The decentralized web awaits</p>
                     </div>
-                    <Link to="/home">
-                        <Button name="Connect to Wallet"/>
-                    </Link>
+                        <Button name={connButtonText} onClick={connectWalletHandler} />
+                    <br/>
+                    {errorMessage}
                 </div>
+
             </div>
         </div>
         <Outlet/>
